8e55c6ad2e20746584b3545b9ab50c886aeecec2
Iwarilama <pepplejoshuatams@gmail.com>
2023-01-15 01:00:43 -0700

    Tested out starting AST code

---
+++ src/ast.rs
@@ -0,0 +1,43 @@
+#[derive(Debug, Clone)]
+pub struct Span {
+    pub file: String,
+    pub line: i32,
+    pub col: i32,
+    pub flat_index_start: i32,
+    pub length: usize,
+}
+
+#[derive(Debug, Clone)]
+pub enum AST {
+    Number { num: i64, span: Span },
+    Identifier { name: String, span: Span },
+}
+
+impl AST {
+    pub fn equals(&self, other: &AST) -> bool {
+        match (self, other) {
+            (
+                AST::Number { ref num, span: _ },
+                AST::Number {
+                    num: ref onum,
+                    span: _,
+                },
+            ) => num == onum,
+            (
+                AST::Identifier { ref name, span: _ },
+                AST::Identifier {
+                    name: ref oname,
+                    span: _,
+                },
+            ) => name == oname,
+            _ => false,
+        }
+    }
+
+    pub fn get_span(&self) -> Span {
+        match self {
+            AST::Number { num: _, ref span } => span.clone(),
+            AST::Identifier { name: _, ref span } => span.clone(),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+mod ast;
+
+use ast::{Span, AST};
+
+fn main() {
+    let a = AST::Number {
+        num: 3,
+        span: Span {
+            file: "garb.txt".into(),
+            line: 1,
+            col: 1,
+            flat_index_start: 1,
+            length: 1,
+        },
+    };
+
+    let b = AST::Number {
+        num: 4,
+        span: Span {
+            file: "garb.txt".into(),
+            line: 1,
+            col: 1,
+            flat_index_start: 1,
+            length: 1,
+        },
+    };
+
+    let c = AST::Identifier {
+        name: "id".into(),
+        span: Span {
+            file: "garb.txt".into(),
+            line: 1,
+            col: 1,
+            flat_index_start: 1,
+            length: 1,
+        },
+    };
+
+    let d = a.clone();
+
+    let e = AST::Identifier {
+        name: "uid".into(),
+        span: Span {
+            file: "garb.txt".into(),
+            line: 1,
+            col: 1,
+            flat_index_start: 1,
+            length: 1,
+        },
+    };
+
+    assert!(!a.equals(&b));
+    assert!(a.equals(&d));
+    assert!(!c.equals(&e));
+    assert!(c.equals(&c));
+
+    println!("{:#?}", a.get_span());
+}
