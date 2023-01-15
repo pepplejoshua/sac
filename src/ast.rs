97332f0034ea20b209a4b00647ee9c165830b11e
Iwarilama <pepplejoshuatams@gmail.com>
2023-01-15 01:57:41 -0700

    Added Equals and NEquals AST code

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,39 @@
+use std::cmp;
+
 #[derive(Debug, Clone)]
 pub struct Span {
     pub file: String,
-    pub line: i32,
-    pub col: i32,
-    pub flat_index_start: i32,
-    pub length: usize,
+    pub start_line: i32,
+    pub start_col: i32,
+    pub end_line: i32,
+    pub end_col: i32,
+}
+
+impl Span {
+    pub fn new_dud() -> Span {
+        Span {
+            file: "garb.txt".into(),
+            start_line: 1,
+            start_col: 1,
+            end_line: 1,
+            end_col: 1,
+        }
+    }
+
+    pub fn merge_with(&self, other: &Span) -> Span {
+        let start_line = cmp::min(self.start_line, other.start_line);
+        let start_col = cmp::min(self.start_col, other.start_col);
+        let end_line = cmp::max(self.end_line, other.end_line);
+        let end_col = cmp::max(self.end_col, other.end_col);
+
+        Span {
+            file: self.file.clone(),
+            start_line,
+            start_col,
+            end_line,
+            end_col,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -12,6 +41,8 @@ pub enum AST {
     Number { num: i64, span: Span },
     Identifier { name: String, span: Span },
     Not { target: Box<AST>, span: Span },
+    Equals { lhs: Box<AST>, rhs: Box<AST> },
+    NEquals { lhs: Box<AST>, rhs: Box<AST> },
 }
 
 impl AST {
@@ -38,6 +69,20 @@ impl AST {
                     span: _,
                 },
             ) => target.equals(otarget),
+            (
+                AST::Equals { lhs, rhs },
+                AST::Equals {
+                    lhs: olhs,
+                    rhs: orhs,
+                },
+            ) => lhs.equals(olhs) && rhs.equals(orhs),
+            (
+                AST::NEquals { lhs, rhs },
+                AST::NEquals {
+                    lhs: olhs,
+                    rhs: orhs,
+                },
+            ) => lhs.equals(olhs) && rhs.equals(orhs),
             _ => false,
         }
     }
@@ -50,6 +95,8 @@ impl AST {
                 target: _,
                 ref span,
             } => span.clone(),
+            AST::Equals { lhs, rhs } => lhs.get_span().merge_with(&rhs.get_span()),
+            AST::NEquals { lhs, rhs } => lhs.get_span().merge_with(&rhs.get_span()),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,80 +3,59 @@ mod ast;
 use ast::{Span, AST};
 
 fn main() {
+    let mut moded_span = Span::new_dud();
+    moded_span.start_line = 1;
+    moded_span.start_col = 5;
+    moded_span.end_line = 7;
+    moded_span.end_col = 8;
+
     let a = AST::Number {
         num: 3,
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: Span::new_dud(),
     };
 
     let b = AST::Number {
         num: 4,
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: moded_span,
     };
 
     let c = AST::Identifier {
         name: "id".into(),
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: Span::new_dud(),
     };
 
     let d = a.clone();
 
     let e = AST::Identifier {
         name: "uid".into(),
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: Span::new_dud(),
     };
 
     let f = AST::Not {
         target: Box::new(b.clone()),
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: Span::new_dud(),
     };
 
     let g = AST::Not {
         target: Box::new(b.clone()),
-        span: Span {
-            file: "garb.txt".into(),
-            line: 1,
-            col: 1,
-            flat_index_start: 1,
-            length: 1,
-        },
+        span: Span::new_dud(),
+    };
+
+    let h = AST::Equals {
+        lhs: Box::new(a.clone()),
+        rhs: Box::new(b.clone()),
+    };
+
+    let i = AST::NEquals {
+        lhs: Box::new(c.clone()),
+        rhs: Box::new(e.clone()),
     };
 
     assert!(!a.equals(&b));
     assert!(a.equals(&d));
     assert!(!c.equals(&e));
     assert!(c.equals(&c));
-
     assert!(f.equals(&g));
 
-    println!("{:#?}", a.get_span());
+    println!("{:#?}\n{:#?}", h.get_span(), i.get_span());
 }
