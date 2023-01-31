3837b5401f0ca637cbde509313183b140d8db2a9
Iwarilama <pepplejoshuatams@gmail.com>
2023-01-30 21:32:54 -0700

    Impl'd and tested atom

--- src/frontend/sac_parser.rs
+++ src/frontend/sac_parser.rs
@@ -30,7 +30,7 @@ fn test_id() {
 
 #[allow(dead_code)]
 fn expression(_input: &str) -> ParseResult<AST> {
-    id.parse(_input)
+    atom(_input)
 }
 
 #[allow(dead_code)]
@@ -46,7 +46,7 @@ fn ignored(input: &str) -> ParseResult<()> {
 fn args(input: &str) -> ParseResult<Vec<AST>> {
     expression
         .and_then(|arg| {
-            zero_or_more(literal(",").and_right(ignored).and_right(expression))
+            zero_or_more(sliteral(",").and_right(ignored).and_right(expression))
                 .and_then(move |args| constant(vec![vec![arg.clone()], args].concat()))
         })
         .or(constant(vec![]))
@@ -78,7 +78,112 @@ fn test_args() {
     assert_eq!(args(""), Ok(("", vec![],)))
 }
 
+#[allow(dead_code)]
+fn sliteral(exp: &str) -> impl Parser<String> {
+    ignored.and_right(match_regex(exp))
+}
 
+#[allow(dead_code)]
+fn sident(input: &str) -> ParseResult<String> {
+    ignored.and_right(identifier).parse(input)
+}
+
+#[allow(dead_code)]
+fn call(input: &str) -> ParseResult<AST> {
+    sident
+        .and_then(|callee| {
+            sliteral("[(]").and_right(args.and_then(move |args| {
+                sliteral("[)]").and_right(constant(AST::Call {
+                    called: callee.clone(),
+                    args,
+                    span: Span::new_dud(),
+                }))
+            }))
+        })
+        .parse(input)
+}
+
+#[test]
+fn test_call() {
+    assert_eq!(
+        call("fib(a,      b,      c)"),
+        Ok((
+            "",
+            AST::Call {
+                called: "fib".into(),
+                args: vec![
+                    AST::Identifier {
+                        name: "a".into(),
+                        span: Span::new_dud()
+                    },
+                    AST::Identifier {
+                        name: "b".into(),
+                        span: Span::new_dud()
+                    },
+                    AST::Identifier {
+                        name: "c".into(),
+                        span: Span::new_dud()
+                    }
+                ],
+                span: Span::new_dud()
+            }
+        ))
+    )
+}
+
+#[allow(dead_code)]
+fn number(input: &str) -> ParseResult<AST> {
+    number_i64
+        .map(|num| AST::Number {
+            num,
+            span: Span::new_dud(),
+        })
+        .parse(input)
+}
+
+#[allow(dead_code)]
+fn atom(input: &str) -> ParseResult<AST> {
+    call.or(id)
+        .or(number)
+        .or(sliteral("[(]")
+            .and_right(expression)
+            .and_then(|expr| sliteral("[)]").and_right(constant(expr))))
+        .parse(input)
+}
+
+#[test]
+fn test_atom() {
+    assert_eq!(
+        atom("a"),
+        Ok((
+            "",
+            AST::Identifier {
+                name: "a".into(),
+                span: Span::new_dud(),
+            }
+        ))
+    );
+    assert_eq!(
+        atom("321"),
+        Ok((
+            "",
+            AST::Number {
+                span: Span::new_dud(),
+                num: 321
+            }
+        ))
+    );
+    assert_eq!(
+        atom("(   (321))"),
+        Ok((
+            "",
+            AST::Number {
+                span: Span::new_dud(),
+                num: 321
+            }
+        ))
+    );
+}
 
 #[allow(dead_code, unused_variables, non_snake_case)]
 pub fn parse(input: &str) {}
