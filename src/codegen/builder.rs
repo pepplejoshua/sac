64c6fa26e8e2967e70d633c709dc5c1118d7d776
Joshua Pepple <36272359+pepplejoshua@users.noreply.github.com>
2023-02-02 04:52:20 +0000

    Added environment to track parameter offsets

--- src/codegen/builder.rs
+++ src/codegen/builder.rs
@@ -1,13 +1,47 @@
-use std::{ffi::OsStr, fs, path::Path};
+use std::{collections::HashMap, ffi::OsStr, fs, path::Path};
+
+#[allow(dead_code)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+
+pub struct Context {
+    locals: HashMap<String, i32>,
+    outer: Option<Box<Context>>,
+}
+
+#[allow(dead_code)]
+impl Context {
+    fn n() -> Self {
+        Context {
+            locals: HashMap::new(),
+            outer: None,
+        }
+    }
+
+    fn enter(&self) -> Self {
+        Context {
+            locals: HashMap::new(),
+            outer: Some(Box::new(self.clone())),
+        }
+    }
+
+    fn set(&mut self, local: String, offset: i32) {
+        self.locals.insert(local, offset);
+    }
+}
 
 pub struct Builder {
     src: String,
-    pub lines: Vec<String>,
+    lines: Vec<String>,
+    context: Context,
 }
 
 impl Builder {
     pub fn n(src: String) -> Self {
-        Builder { src, lines: vec![] }
+        Builder {
+            src,
+            lines: vec![],
+            context: Context::n(),
+        }
     }
 
     pub fn add(&mut self, line: &str) {
@@ -27,5 +61,11 @@ impl Builder {
 
     pub fn get_src(&self) -> String {
         fs::read_to_string(&self.src).unwrap()
+    }
+
+    pub fn set_up_env(&mut self, locals: &[String]) {
+        for (i, local) in locals.iter().enumerate() {
+            self.context.set(local.clone(), (4 * i - 16) as i32);
+        }
     }
 }
